a9c91f6adebf1104d28be8f78d23ea87900a551b
longzipeng <longzipeng@foxmail.com>
2024-02-23 18:32:41 +0800

    修复错误

--- globel/channelEvent.d.ts
+++ globel/channelEvent.d.ts
@@ -1,10 +1,8 @@
-// customEvents.d.ts
-
 /**
- * 自定义事件枚举的类型声明
+ * 自定义事件枚举
  */
-export declare enum CustomChannel {
-  window_create,
-  window_move_on,
-  window_position_change,
+export enum CustomChannel {
+  window_create = "custom_window_create", // 窗口新建事件
+  window_move_on = "custom_window_move_on", // 开启窗口移动事件
+  window_position_change = "custom_window_position_change", // 修改窗口的位置
 }
